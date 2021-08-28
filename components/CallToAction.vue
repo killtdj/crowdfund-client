81ee9020661c5c5143e4495a584ba2a4efe7fea6
Denny Danuwijaya <danudenny@gmail.com>
2021-08-28 16:52:14 +0700

    :tada: first init

---
+++ components/CallToAction.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="call-to-action bg-purple-progress pt-64 pb-10">
+        <div class="container mx-auto">
+            <div class="w-full text-center">
+                <h1 class="text-5xl font-semibold text-white">
+                    Easy way to funding
+                    <br />
+                    best idea and innovation
+                </h1>
+                <button
+                    @click="$router.push({ path: '/register' })"
+                    class="
+                        bg-orange-button
+                        hover:bg-green-button
+                        inline-block
+                        px-6
+                        py-4
+                        mt-8
+                        text-lg
+                        font-semibold
+                        text-white
+                        rounded-full
+                    "
+                >
+                    Getting Start
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,81 @@
+<template>
+    <header class="flex items-center">
+        <div style="height: 54px" class="pr-5">
+            <img src="/logo.svg" alt="logo" class="h-full" />
+        </div>
+        <ul class="flex items-center">
+            <li>
+                <nuxt-link
+                    class="hover:text-teal-500 px-4 py-3 text-lg text-white"
+                    to="/"
+                    >Home</nuxt-link
+                >
+            </li>
+            <li>
+                <a
+                    class="hover:text-teal-500 px-4 py-3 text-lg text-white"
+                    href="#"
+                    >Project</a
+                >
+            </li>
+            <li>
+                <a
+                    class="hover:text-teal-500 px-4 py-3 text-lg text-white"
+                    href="#"
+                    >Features</a
+                >
+            </li>
+            <li>
+                <a
+                    class="hover:text-teal-500 px-4 py-3 text-lg text-white"
+                    href="#"
+                    >Success Stories</a
+                >
+            </li>
+        </ul>
+        <ul class="flex items-center mt-2 ml-auto">
+            <li>
+                <nuxt-link
+                    to="/register"
+                    class="
+                        hover:bg-white hover:bg-opacity-25
+                        inline-block
+                        w-40
+                        px-6
+                        py-1
+                        mr-4
+                        text-lg
+                        font-light
+                        text-center text-white
+                        bg-transparent
+                        border border-white
+                        rounded-full
+                    "
+                >
+                    Sign Up
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link
+                    to="/login"
+                    class="
+                        inline-block
+                        bg-transparent
+                        border-white border
+                        hover:bg-white hover:bg-opacity-25
+                        text-white
+                        font-light
+                        w-40
+                        text-center
+                        px-6
+                        py-1
+                        text-lg
+                        rounded-full
+                    "
+                >
+                    My Account
+                </nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
